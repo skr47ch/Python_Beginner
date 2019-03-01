a5becd1d041721a4ad9184cb645f1928b02a25c4
skr47ch <4n0m4nd3R>
2019-03-01 23:23:21 +1100

    13th problem solved

--- 13_fibonacci_sequence.py
+++ 13_fibonacci_sequence.py
@@ -1,3 +1,7 @@
+# Write a program that asks the user how many Fibonacci numbers to generate and then generates them.
+# Take this opportunity to think about how you can use functions.
+# Make sure to ask the user to enter the number of numbers in the sequence to generate
+
 
 def fibonacci(count):
     x = 0
